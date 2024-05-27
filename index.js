a5a7ddba2825885a1dbcbadc5eb9bc21289862c6
SagarKumar977 <sag197348@gmail.com>
2024-05-27 09:45:48 +0530

    update

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+
+const app = express();
+
+
+
+app.get('/',(req,res)=>{
+    res.send('<h1>Welcome</h1>')
+})
+
+app.listen(5000,()=>{
+    console.log("Server running at http://localhost:5000");
+})
